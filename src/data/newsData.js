b80b04d501ef49d7b0e60f3a7365744ad043f00f
michelmnds <m1tchofc@gmail.com>
2024-01-29 15:58:48 +0000

    feat: added new newsData

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -1,18 +1,145 @@
 export const newsData = [
   {
-    id: 1,
-    name: "Aula ao Ar Livre",
-    date: "11/07/2023",
+    id: 21,
+    name: "Circo dos Sonhos",
+    date: "10/01/2024",
     schedule: [
       {
-        day: "11/07/2023",
+        day: "14/01/2024",
         time: "17:00",
-        location: "Parque da cidade",
+        location: "Cine Teatro Avenida ",
+      },
+    ],
+    description:
+      "O “Circo dos Sonhos” privilegia a animação e os sorrisos, sempre com uma atmosfera festiva. Leva o público a brincar, a abrir o coração e a fazer parte. Com muito brilho, os bailarinos fundem-se com os músicos, os atores dão vida a esta história original e todos compõem uma sinfonia de ritmos. Neste lugar mágico esperamos por ti, para que possas sentir as cores vibrantes deste espetáculo. Are you ready to activate your soul?",
+    image: "https://i.imgur.com/iCriVFr.png",
+    link: "https://www.youtube.com/watch?v=PuhmxZOjREU",
+  },
+  {
+    id: 20,
+    name: "ATL Natal",
+    date: "10/12/2023",
+    schedule: [
+      {
+        day: "18/12/2023 - 29/12/2023",
+        time: "19:00",
+        location: "Estúdio Active Soul e Fábrica da Criatividade",
+      },
+    ],
+    description:
+      "Depois do sucesso do Active Kids de Páscoa, o ATL mais artístico da cidade está de volta, desta vez para preencher a época natalícia dos mais novos! Mais uma vez, o Teatro Tramédia junta-se ao Active Soul com o ATL onde podes divertir-te, aprender, desenvolver a tua criatividade e PARTICIPAR NUM ESPETÁCULO",
+    image: "https://i.imgur.com/O3UmBwd.png",
+    link: "https://www.youtube.com/watch?v=xZ7i5vrnuzI&t=13s",
+  },
+  {
+    id: 19,
+    name: "Dia Nacional do Pijama",
+    date: "18/11/2023",
+    schedule: [
+      {
+        day: "20/11/2023",
+        time: "15:00",
+        location: "Estúdio Active Soul",
+      },
+    ],
+    description:
+      "O Dia Nacional do Pijama é um dia educativo e solidário! 👧🏻 Este é o dia que marca a importância de lembrar, a todos que “uma criança tem direito a crescer numa família”. Celebrámos este dia junto dos nossos alunos, partilhem este miminho",
+    image: "https://i.imgur.com/PzNcEn1.png",
+    link: "https://www.instagram.com/reel/Cz4KFWktQDu/",
+  },
+  {
+    id: 18,
+    name: "Mostra o que Vales 5",
+    date: "07/11/2023",
+    schedule: [
+      {
+        day: "05/11/2023",
+        time: "15:00",
+        location: "La Vie Guarda",
+      },
+    ],
+    description:
+      "'O sonho comanda a vida' já dizia Fernando Pessoa... E é sobre isso que falamos, sobre o sonho destas 2 meninas que temos o privilégio de ver crescer! Mais uma vez o esforço, trabalho e dedicação deram frutos e na competição “Mostra o que vales 5”, na Guarda alcançaram uma belíssima qualificação",
+    image: "https://i.imgur.com/83sY777.png",
+    link: "",
+  },
+  {
+    id: 17,
+    name: "Halloween",
+    date: "27/10/2023",
+    schedule: [
+      {
+        day: "31/10/2023",
+        time: "18:00",
+        location: "Estúdio Active Soul",
+      },
+    ],
+    description:
+      "O Estúdio Active Soul proporcionou aos seus alunos um final de tarde diferente. Obrigada a todos por terem participado em mais uma iniciativa",
+    image: "https://i.imgur.com/LWJ7HMJ.png",
+    link: "",
+  },
+  {
+    id: 16,
+    name: "Semana da Juventude",
+    date: "17/09/2023",
+    schedule: [
+      {
+        day: "23/09/2023",
+        time: "15:00",
+        location: "Parque Cruz Montalvão",
+      },
+    ],
+    description:
+      "Vamos participar da Semana da Juventude com o espetáculo 'Contador de Histórias'. Encontramo-nos lá",
+    image: "https://i.imgur.com/N3ILSDR.png",
+    link: "https://www.youtube.com/watch?v=oGGn7ISFTlg&t=4s&ab_channel=ActiveSoul",
+  },
+  {
+    id: 15,
+    name: "Festival +Solidário",
+    date: "04/08/2023",
+    schedule: [
+      {
+        day: "12/08/2023",
+        time: "18:00",
+        location: "",
+      },
+    ],
+    description:
+      "Vamos estar presentes no Festival Mais Solidário com muita animação e dança",
+    image: "https://i.imgur.com/eBqOsMj.png",
+    link: "https://www.youtube.com/watch?v=aTjJUz_NNoU&ab_channel=ActiveSoul",
+  },
+  {
+    id: 14,
+    name: "DJ Giga",
+    date: "01/08/2023",
+    schedule: [
+      {
+        day: "07/08/2023",
+        time: "18:00",
+        location: "Malpica do Tejo ",
+      },
+    ],
+    description: "Vamos partilhar o palco com o DJ Giga, esperamos por ti.",
+    image: "https://i.imgur.com/GzdEgLd.png",
+    link: "",
+  },
+  {
+    id: 13,
+    name: "Aula ao Ar Livre",
+    date: "10/07/2023",
+    schedule: [
+      {
+        day: "10/07/2023",
+        time: "19:30",
+        location: "Parque Cruz Montalvão",
       },
       {
         day: "31/07/2023",
         time: "19:30",
-        location: "Parque da cidade",
+        location: "Parque Cruz Montalvão",
       },
     ],
     description:
@@ -21,13 +148,29 @@ export const newsData = [
     link: "https://www.facebook.com/activesoulstudio/videos/6394273757318102/",
   },
   {
-    id: 2,
+    id: 12,
+    name: "Contadora de Histórias ",
+    date: "18/06/2023",
+    schedule: [
+      {
+        day: "18/06/2023",
+        time: "17:00",
+        location: "Cine Teatro Avenida de Castelo Branco",
+      },
+    ],
+    description:
+      "“Contadora de histórias” foi o 1° espetáculo de final de ano da Active Soul. A dança e as palavras tomaram conta desta hora de espetáculo repleto de movimento, histórias e muita magia, acreditar que nada é impossível e que o tempo pode ser o nosso melhor aliado.",
+    image: "https://i.imgur.com/bSpqdSX.png",
+    link: "https://www.instagram.com/reel/CtyfZpeO6xP/",
+  },
+  {
+    id: 11,
     name: "Dia da Criança | Teatro Tramédia + Active Soul",
     date: "02/06/2023",
     schedule: [
       {
         day: "01/06/2023",
-        time: "17:00",
+        time: "17:30",
         location: "Parque da cidade",
       },
     ],
@@ -37,30 +180,30 @@ export const newsData = [
     link: "https://www.facebook.com/teatrotramedia/videos/652959506675294/",
   },
   {
-    id: 3,
+    id: 10,
     name: "Dia Mundial da Dança",
     date: "30/04/2023",
     schedule: [
       {
         day: "29/04/2023",
-        time: "15:00",
+        time: "16:00",
         location: "Docas",
       },
     ],
     description:
       "Juntámos cerca de 70 pessoas e saímos à rua para celebrar todos juntos este dia tão especial! Todos aqui estiveram juntos pela paixão a esta grande arte que é a dança. Dancem, dancem muito… porque quem dança é mesmo mais feliz!",
     image: "https://i.imgur.com/Z60jJcx.jpg",
-    link: "https://www.facebook.com/activesoulstudio/posts/236384865642012",
+    link: "https://www.instagram.com/reel/CrojpvRPueH/",
   },
   {
-    id: 4,
+    id: 9,
     name: "ATL Páscoa",
     date: "07/04/2023",
     schedule: [
       {
-        day: "09/04/2023",
+        day: "03/04/2023 - 14/04/2023",
         time: "19:00",
-        location: "Cine Teatro",
+        location: "Estúdio Active Soul e Fábrica da Criatividade",
       },
     ],
     description:
@@ -69,7 +212,7 @@ export const newsData = [
     link: "https://www.youtube.com/watch?v=tezTAfNg5a0&t=545s&ab_channel=ActiveSoul",
   },
   {
-    id: 5,
+    id: 8,
     name: "Dia Mundial da Atividade Física com a Apef Castelo Branco",
     date: "08/04/2024",
     schedule: [
@@ -85,29 +228,34 @@ export const newsData = [
     link: "https://www.facebook.com/apef.castelobranco/videos/798096874506071/",
   },
   {
-    id: 6,
+    id: 7,
     name: "Alice no País das Maravilhas",
     date: "07/04/2023",
     schedule: [
       {
         day: "07/04/2023",
-        time: "17:00",
-        location: "Docas",
+        time: "16:00",
+        location: "Mercadinho da Páscoa",
+      },
+      {
+        day: "09/04/2023",
+        time: "16:00",
+        location: "Mercadinho da Páscoa",
       },
     ],
     description:
       "Em um espetáculo de dança envolvente inspirado em 'Alice no País das Maravilhas', os bailarinos ganham vida, transportando o público para o mundo mágico e surreal criado por Lewis Carroll. A coreografia dinâmica e os figurinos deslumbrantes dão vida aos personagens icônicos, como a própria Alice, o Chapeleiro Maluco e a Rainha de Copas. A trilha sonora encantadora guia os dançarinos através de cenários deslumbrantes, recriando momentos clássicos da história. Com movimentos graciosos e uma atmosfera encantadora, o espetáculo de dança oferece uma interpretação cativante e visualmente deslumbrante do amado conto de fadas de Carroll.",
     image: "https://i.imgur.com/W30dAsi.jpg",
-    link: "",
+    link: "https://www.instagram.com/reel/CrD3C-SPMev/",
   },
   {
-    id: 7,
+    id: 6,
     name: "Matiné Dançante",
     date: "18/03/2023",
     schedule: [
       {
         day: "18/03/2023",
-        time: "09:00",
+        time: "15:00 - 17:00",
         location: "Estúdio Active Soul",
       },
     ],
@@ -117,13 +265,13 @@ export const newsData = [
     link: "",
   },
   {
-    id: 8,
-    name: "Dia da Mulher",
+    id: 5,
+    name: "Dia Internacional da Mulher",
     date: "11/03/2023",
     schedule: [
       {
         day: "11/03/2023",
-        time: "17:00",
+        time: "21:30",
         location: "Associação Juvenil Ribeiro das Perdizes",
       },
     ],
@@ -133,14 +281,14 @@ export const newsData = [
     link: "",
   },
   {
-    id: 9,
+    id: 4,
     name: "Carnaval com A Praça",
     date: "20/02/2023",
     schedule: [
       {
         day: "20/02/2023",
-        time: "19:00",
-        location: "A Praça Fit",
+        time: "18:00",
+        location: "Ginásio A Praça",
       },
     ],
     description:
@@ -149,23 +297,7 @@ export const newsData = [
     link: "",
   },
   {
-    id: 10,
-    name: "Workshop Dance Hall",
-    date: "30/09/2022",
-    schedule: [
-      {
-        day: "30/09/2022",
-        time: "15:00",
-        location: "Estúdio Active Soul",
-      },
-    ],
-    description:
-      "A Active Soul anuncia um workshop imperdível para os amantes da dança: o Workshop Dance Hall! Este evento envolvente oferecerá uma experiência única, explorando os ritmos vibrantes e a cultura do Dance Hall. Conduzido por instrutores talentosos, o workshop promete aprimorar habilidades de dança, proporcionar momentos de diversão e celebrar a energia contagiante do Dance Hall.",
-    image: "https://i.imgur.com/zStKdyu.jpg",
-    link: "",
-  },
-  {
-    id: 11,
+    id: 3,
     name: "Pipoto Maroto",
     date: "17/12/2022",
     schedule: [
@@ -179,5 +311,37 @@ export const newsData = [
       "A Active Soul promete encantar a todos nesta temporada festiva com o espetáculo natalino 'Pipoto Maroto'. Repleto de magia, música e personagens cativantes, o espetáculo promete ser um evento imperdível para toda a família. Com coreografias envolventes e figurinos deslumbrantes, 'Pipoto Maroto' transportará o público para um mundo encantado, onde a alegria do Natal se manifesta de maneiras únicas.",
     image: "https://i.imgur.com/f8PNSa4.jpg",
     link: "https://www.youtube.com/watch?v=ba7o7XYgBK4&t=26s&ab_channel=ActiveSoul",
+  },
+  {
+    id: 2,
+    name: "Workshop “As minhas emoções”",
+    date: "20/11/2022",
+    schedule: [
+      {
+        day: "26/11/2022",
+        time: "10:00 - 11:00 & 11:00 - 12:00",
+        location: "Estúdio Active Soul",
+      },
+    ],
+    description:
+      "A Active Soul promete encantar a todos nesta temporada festiva com o espetáculo natalino 'Pipoto Maroto'. Repleto de magia, música e personagens cativantes, o espetáculo promete ser um evento imperdível para toda a família. Com coreografias envolventes e figurinos deslumbrantes, 'Pipoto Maroto' transportará o público para um mundo encantado, onde a alegria do Natal se manifesta de maneiras únicas.",
+    image: "https://i.imgur.com/f8PNSa4.jpg",
+    link: "",
+  },
+  {
+    id: 1,
+    name: "Workshop Dance Hall",
+    date: "30/09/2022",
+    schedule: [
+      {
+        day: "01/10/2022",
+        time: "15:00 - 17:00",
+        location: "Estúdio Active Soul",
+      },
+    ],
+    description:
+      "A Active Soul anuncia um workshop imperdível para os amantes da dança: o Workshop Dance Hall! Este evento envolvente oferecerá uma experiência única, explorando os ritmos vibrantes e a cultura do Dance Hall. Conduzido por instrutores talentosos, o workshop promete aprimorar habilidades de dança, proporcionar momentos de diversão e celebrar a energia contagiante do Dance Hall.",
+    image: "https://i.imgur.com/zStKdyu.jpg",
+    link: "https://www.instagram.com/reel/CjNU3MCDYWa/",
   },
 ];
