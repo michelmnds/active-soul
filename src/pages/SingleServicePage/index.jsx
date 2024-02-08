f054bd12166fd5f359be63e1da5efbd6700526ea
michelmnds <m1tchofc@gmail.com>
2024-02-08 14:08:10 +0000

    feat: services page done

--- src/pages/SingleServicePage/index.jsx
+++ src/pages/SingleServicePage/index.jsx
@@ -20,21 +20,21 @@ export const SingleServicePage = ({ menu }) => {
       ".<br />"
     );
     return (
-      <div className={`singleDanceContainer ${menu ? "move" : ""}`}>
+      <div className={`singleServiceContainer ${menu ? "move" : ""}`}>
         <div
-          className="singleDanceImg"
+          className="singleServiceImg"
           style={{ backgroundImage: `url(${currentClass.image})` }}
         />
 
-        <div className="singleDanceInfos">
-          <h2 className="singleDanceName">{currentClass.title}</h2>
+        <div className="singleServiceInfos">
+          <h2 className="singleServiceName">{currentClass.title}</h2>
 
           <span
-            className="singleDanceDesc"
+            className="singleServiceDesc"
             dangerouslySetInnerHTML={{ __html: formattedDescription }}
           ></span>
 
-          <a href="#" className="singleDanceBtn">
+          <a href="#" className="singleServiceBtn">
             CONTACTE-NOS
           </a>
         </div>
