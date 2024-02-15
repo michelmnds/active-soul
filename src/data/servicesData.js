3c3d9534c46e23f48fcd37f6e4b929bffe235084
michelmnds <m1tchofc@gmail.com>
2024-02-15 14:15:34 +0000

    fix: gitignore

---
+++ src/data/servicesData.js
@@ -0,0 +1,21 @@
+export const servicesData = [
+  {
+    id: 1,
+    title: "Agenciamento",
+    description: `Os nossos bailarinos adaptam-se a todas as exigências e diversidades que o sector de eventos exige. Quer tornar o seu evento/espetáculo único e inesquecível? Os nossos bailarinos fazem-no com qualidade, profissionalismo e dedicação. Para os bailarinos: Queres fazer parte desta equipa?`,
+    image: "https://i.imgur.com/NwEKoJg.jpg",
+  },
+  {
+    id: 2,
+    title: "Casamentos",
+    description: `Podem esperar a criação de uma coreografia personalizada, a respetiva edição musical e as aulas de preparação para a dança. O Active Soul dedica-se exclusivamente a vocês. Tudo é criado e planeado para oferecer uma experiência incrível. TORNA O TEU CASAMENTO ÚNICO E ESPECIAL!`,
+    image: "https://i.imgur.com/kl2JIqp.png",
+  },
+
+  {
+    id: 3,
+    title: "Eventos",
+    description: `• Espetáculos de Dança, Música, Teatro e Interação*• Espetáculos Infantis*• Eventos Privados e Corporativos*• Eventos Desportivos e Culturais*• Activações de Marca*• Flashmob*• Team Building* A nossa equipa é composta por criativos e fornecedores que fazem acontecer qualquer tipo de evento. Criamos eventos à medida de cada cliente, produzindo e dando o melhor da nossa equipa em cada projeto!`,
+    image: "https://i.imgur.com/mBuvBGU.jpg",
+  },
+];
---
+++ src/data/teamData.js
@@ -0,0 +1,45 @@
+export const teamData = [
+  {
+    id: 1,
+    image: "https://i.imgur.com/Tjtpt4L.jpg",
+    name: "Patricia Lourenço",
+    modality:
+      "Diretora Active Soul, Professora de Contemporâneo, Ballet e Personal Trainer",
+  },
+  {
+    id: 2,
+    image: "https://i.imgur.com/mF2NrNE.png",
+    name: "Sandra Vicente",
+    modality: "Professora de Pilates, GAP, Circuito e Personal Trainer",
+  },
+  {
+    id: 3,
+    image: "https://i.imgur.com/JastfiI.png",
+    name: "Andreia Lemos",
+    modality: "Diretora Artística, Project Manager & Wedding Planner",
+  },
+  {
+    id: 4,
+    image: "https://i.imgur.com/kuODAQ7.png",
+    name: "Sofia Ferreira",
+    modality: "Professora de Jazz",
+  },
+  {
+    id: 5,
+    image: "https://i.imgur.com/LmgrPA7.png",
+    name: "Leonor Farias",
+    modality: "Professora Dance Kids",
+  },
+  {
+    id: 6,
+    image: "https://i.imgur.com/A4ou8rZ.png",
+    name: "Fábio Sscola",
+    modality: "Professor de Afro latinas",
+  },
+  {
+    id: 7,
+    image: "https://i.imgur.com/t9PtReL.jpg",
+    name: "Andreia Gabriel",
+    modality: "Professora de Meditação",
+  },
+];
