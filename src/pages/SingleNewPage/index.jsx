df4f79d2d536952b3b346276c2dbe04635beb942
michelmnds <m1tchofc@gmail.com>
2024-03-20 11:13:32 +0000

    ª

--- src/pages/SingleNewPage/index.jsx
+++ src/pages/SingleNewPage/index.jsx
@@ -1,16 +1,90 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import "./style.css";
 import { useParams } from "react-router-dom";
+import { useRef } from "react";
 import { newsData } from "../../data/newsData";
 import { useEffect, useState } from "react";
+import { Carousel } from "@mantine/carousel";
+import { Image } from "@mantine/core";
+import classes from "./Demo.module.css";
+import { IconArrowRight, IconArrowLeft } from "@tabler/icons-react";
+import Autoplay from "embla-carousel-autoplay";
 
 // eslint-disable-next-line react/prop-types
 export const SingleNewPage = ({ menu }) => {
+  const autoplay = useRef(Autoplay({ delay: 3500 }));
   const { currentNewId } = useParams();
   const [currentNew, setCurrentNew] = useState({});
+  const [carouselReady, setCarouselReady] = useState(false);
+
   useEffect(() => {
     setCurrentNew(newsData.find((currentNew) => currentNew.id == currentNewId));
   }, []);
+
+  useEffect(() => {
+    if (currentNewId == 25) {
+      const timeout = setTimeout(() => {
+        setCarouselReady(true);
+      }, 100);
+
+      return () => clearTimeout(timeout);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (carouselReady) {
+      autoplay.current.play();
+    }
+  }, [carouselReady]);
+
+  const imageUrls = [
+    "https://i.imgur.com/HFKxFRt.jpg",
+    "https://i.imgur.com/YqCGKBH.jpg",
+    "https://i.imgur.com/4JYgjO1.jpg",
+    "https://i.imgur.com/9VQfTf1.jpg",
+    "https://i.imgur.com/MH8ogmf.jpg",
+  ];
+
+  function CarouselComponent() {
+    const slides = imageUrls.map((image) => (
+      <Carousel.Slide key={image}>
+        <Image style={{ height: `${100}%` }} src={image} />
+      </Carousel.Slide>
+    ));
+    return (
+      <Carousel
+        classNames={classes}
+        className={`${menu ? "move" : ""}`}
+        withIndicators
+        loop
+        plugins={[autoplay.current]}
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+        nextControlIcon={
+          <IconArrowRight
+            style={{
+              width: `rem(${16})`,
+              height: `rem(${16})`,
+              backgroundColor: "white",
+              borderRadius: 12,
+            }}
+          />
+        }
+        previousControlIcon={
+          <IconArrowLeft
+            style={{
+              width: `rem(${16})`,
+              height: `rem(${16})`,
+              backgroundColor: "white",
+              borderRadius: 12,
+            }}
+          />
+        }
+      >
+        {slides}
+      </Carousel>
+    );
+  }
 
   if (currentNew.id) {
     const formattedDescription = currentNew.description.replace(
@@ -101,6 +175,8 @@ export const SingleNewPage = ({ menu }) => {
               );
             })}
           </section>
+
+          <CarouselComponent />
 
           <a
             href="https://wa.me/message/P62IN6OYSZ5YN1"
