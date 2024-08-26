9b2a9f996d45cd37658a73a067b8f1c951850e19
michelmnds <m1tchofc@gmail.com>
2024-08-26 12:50:34 +0100

    feat: pic changes

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -1,51 +1,51 @@
 export const teamData = [
   {
     id: 1,
-    image: "https://i.imgur.com/EVAYTmR.png",
+    image: "https://i.imgur.com/X9Y2KND.png",
     name: "Patrícia Lourenço",
     modality:
       "Diretora Active Soul, Professora de Contemporâneo, Ballet e Personal Trainer",
   },
   {
     id: 2,
-    image: "https://i.imgur.com/83mDn3v.png",
+    image: "https://i.imgur.com/sChHPMn.png",
     name: "Andreia Lemos",
     modality: "Diretora Artística, Project Manager & Wedding Planner",
   },
   {
     id: 3,
-    image: "https://i.imgur.com/quw2nQw.png",
+    image: "https://i.imgur.com/XOyFDmG.png",
     name: "Sandra Vicente",
     modality:
       "Professora de Pilates, GAP, Circuito, Preparação Física para Bailarinos e Personal Trainer",
   },
   {
     id: 4,
-    image: "https://i.imgur.com/YbcPDNL.png",
+    image: "https://i.imgur.com/g8g0Zjq.png",
     name: "Sofia Ferreira",
     modality: "Professora de Ballet e Jazz",
   },
   {
     id: 5,
-    image: "https://i.imgur.com/dA8e9Aw.png",
+    image: "https://i.imgur.com/GuUORgk.png",
     name: "Leonor Farias",
     modality: "Professora de Dance Kids e Pop Dance",
   },
   {
     id: 6,
-    image: "https://i.imgur.com/esTvQ2X.png",
+    image: "https://i.imgur.com/Pwp2Ehz.png",
     name: "Fábio Sscola",
     modality: "Professor de Afro Latinas e Ritmos",
   },
   {
     id: 7,
-    image: "https://i.imgur.com/xPuYIZr.png",
+    image: "https://i.imgur.com/qYH37uL.png",
     name: "Catarina Reino",
     modality: "Professora de Teatro Musical",
   },
   {
     id: 8,
-    image: "https://i.imgur.com/7L32SAr.png",
+    image: "https://i.imgur.com/xxMZMTe.png",
     name: "Ana Mota",
     modality: "Professora de Teatro Musical",
   },
