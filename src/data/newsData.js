099cce8d9e0852386889082b605b34bab48d550f
michelmnds <m1tchofc@gmail.com>
2024-11-16 19:10:45 +0000

    fix: photo

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -13,7 +13,7 @@ export const newsData = [
     ],
     description:
       "É com grande entusiasmo que anunciamos o 2º Interior TeamGym Castelo Branco, uma competição a nível nacional que teve lugar no dia 16 de novembro de 2024, no pavilhão da Escola Afonso de Paiva, em Castelo Branco e alguns alunos da turma de Contemporâneo II participaram na abertura do mesmo.\nAgradecemos desde já o convite por parte da ZakiGym e desejamos todo o sucesso no seu percurso.",
-    image: "https://i.imgur.com/Me4py5c.jpg",
+    image: "https://i.imgur.com/oGQ0nm0.jpg",
     link: "",
     linkType: "",
   },
