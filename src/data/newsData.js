be143c0d081e9fab4bdbd0c6cc3be08fb835563a
Michel Mendes <m1tchofc@gmail.com>
2026-01-24 17:17:54 +0000

    fix: date

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -3,18 +3,18 @@ export const newsData = [
     id: 70,
     name: "",
     title: "Brisamar",
-    date: "20/12/2025",
+    date: "01/02/2026",
     schedule: [
       {
-        day: "01/02/2025",
+        day: "01/02/2026",
         time: "15:00 - 16:30",
         location: "Cine Teatro Avenida de Castelo Branco",
       },
       {
-        day: "01/02/2025",
+        day: "01/02/2026",
         time: "17:00 - 18:30",
         location: "Cine Teatro Avenida de Castelo Branco",
-      }
+      },
     ],
     description:
       "Brisamar é uma viagem sensível e encantadora ao coração do oceano. Através da dança, do teatro e da música, somos convidados a mergulhar num mar cheio de vida, histórias e emoções, onde cada onda nos lembra a beleza e a fragilidade do nosso planeta.\nGuiados pelo Pescador, pelo Tritão e por outras personagens do mar, descobrimos um oceano que fala, sente e pede ajuda.\nEntre corais, peixes, aves e tartarugas, o espetáculo aborda de forma lúdica e consciente temas como a poluição, a reciclagem, a proteção dos animais marinhos e a importância das pequenas ações de cada um de nós.\nMais do que um espetáculo, Brisamar é um convite à reflexão, à responsabilidade e à esperança* Uma celebração do mar como casa comum, fonte de vida e inspiração, lembrando-nos que cuidar do oceano é cuidar de nós próprios.\nPorque quando o mar sorri, o mundo inteiro respira melhor.",
