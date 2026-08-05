64628d2ff4215e2de0809285a68d8941f8a4b969
Michel Mendes <m1tchofc@gmail.com>
2026-08-05 13:00:03 +0100

    feat: add Tailwind design foundation

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import App from "@/App.jsx";
+import "@/index.css";
 import "./reset.css";
 import "./globalStyle.css";
 
